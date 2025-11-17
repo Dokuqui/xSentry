78c520549791bf3d9bedade064f29e185b630241
Illia <doku_isv@icloud.com>
2025-11-17 11:07:38 +0100

    feat: add simple aws detection

--- cmd/xSentry/main.go
+++ cmd/xSentry/main.go
@@ -1,7 +1,39 @@
-package xSentry
+package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
 
 func main() {
-	fmt.Println("Starting xSentry v0.1...")
+	awsKeyRegex := regexp.MustCompile(`AKIA[A-Z0-9]{16}`)
+
+	foundSecret := false
+	lineNumber := 0
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		lineNumber++
+		line := scanner.Text()
+
+		if awsKeyRegex.MatchString(line) {
+			fmt.Fprintf(os.Stderr, "🚨 [xSentry] Secret found on line %d: AWS Access Key\n", lineNumber)
+			foundSecret = true
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+		os.Exit(2)
+	}
+
+	if foundSecret {
+		os.Exit(1)
+	}
+
+	fmt.Println("✅ [xSentry] No secrets found.")
+	os.Exit(0)
 }
